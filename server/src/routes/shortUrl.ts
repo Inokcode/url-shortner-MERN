a87a9596fbd97d1a0ad6c64d74161d3178b45efd
Inokcode <regilearn@gmail.com>
2024-06-06 10:40:29 +0530

    route path added

--- server/src/routes/shortUrl.ts
+++ server/src/routes/shortUrl.ts
@@ -1,1 +1,9 @@
-import second from "first";
+import express from "express";
+const router = express.Router();
+
+router.post("/shortUrl", createUrl);
+router.get("/shortUrl", getAllUrl);
+router.get("/shortUrl/:id", getUrl);
+router.delete("/shortUrl/id", deleteUrl);
+
+export default router;
