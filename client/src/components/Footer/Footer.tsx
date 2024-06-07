2d8c425dd1f8dac63fc548e0cd8c84517d8f9ee8
Inokcode <regilearn@gmail.com>
2024-06-07 14:20:28 +0530

    header and footer added

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+interface IFooterProps {}
+
+const Footer: React.FunctionComponent<IFooterProps> = () => {
+  return (
+    <div className="bg-slate-900 text-white text-base text-center py-5">
+      Copyright &#169; URLShortner | Dipesh Malvia
+    </div>
+  );
+};
+export default Footer;
